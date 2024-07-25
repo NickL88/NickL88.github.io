693054708775f07113627a1073abd1af5373dfb9
NickL88 <nikola.lisjak.acc@gmail.com>
2024-07-25 07:51:56 +0200

    47

--- root.js
+++ root.js
@@ -3,4 +3,5 @@
 import {prikaz, start} from './javascript/prikaz.js'
 
 document.addEventListener('click',prikaz);
+document.addEventListener('touchstart',prikaz);
 document.addEventListener('DOMContentLoaded',start)
