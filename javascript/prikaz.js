bdc5fca2266184c46905d9b39f84b16dd01334f1
NickL88 <nikola.lisjak.acc@gmail.com>
2024-12-31 16:41:23 +0100

    completed

--- javascript/prikaz.js
+++ javascript/prikaz.js
@@ -103,7 +103,7 @@ function vodilja(event) {
     tocka.x = xcoord = event.offsetX;
     tocka.y = ycoord = event.offsetY;
     //crta.points.appendItem(tocka);
-    tocka.y -=  svg.getBoundingClientRect().height/2;
+    //tocka.y -=  svg.getBoundingClientRect().height/2;
     //crta.points += event.offsetX+","+event.offsetY;
     //console.log(crta.points.getItem(0));
     //console.log("linija:(" + tocka.x+","+ tocka.y + ")");
@@ -138,7 +138,7 @@ var id;
 var check = false;
 
 function play(event) {
-  if ([...event.currentTarget.querySelectorAll("#crta")].some(function (comp) {
+  if ([...event.currentTarget.querySelectorAll("#svg")].some(function (comp) {
     return comp == event.target;
   })) {
     if (!check) {
@@ -146,39 +146,69 @@ function play(event) {
       const crta = document.getElementById("crta");
       //event.target.parentElement.style.backgroundColor="red";
       let pos = 0; //trenutak 0
-      let pos0 = crta.points.getItem(crta.points.length - 1); //dohvacanje zadnje tocke polyline
-      let posx0 = pos0.x; //dohvacanje x koordinate zadnje tocke polyline
-      console.log(posx0);
+      //let pos0 = crta.points.getItem(crta.points.length - 1); //dohvacanje zadnje tocke polyline
+      //let pos0 = crta.points.getItem(crta.points.length - 1); //dohvacanje zadnje tocke polyline
+      //let posx0 = pos0.x; //dohvacanje x koordinate zadnje tocke polyline
+      //console.log(posx0);
       //frame();
       const svg = document.getElementById("svg");
+      const der = document.getElementById("der1");
+      const int = document.getElementById("int");
+      const deriv = document.getElementById("deriv");
+      const integ = document.getElementById("integ");
       const interval = 1;
 
       function frame() {
         pos = pos - interval;
-        let pos1 = crta.points.getItem(crta.points.length - 1);
-        let posx = pos1.x;
-        let posy = pos1.y;
-        console.log("pozicija" + pos1);
-        event.target.parentElement.style.left = pos + 'px'; //element sliding
-        console.log(event.target.parentElement.getBoundingClientRect().width);
-        let width = event.target.parentElement.getBoundingClientRect().width + interval;
+        //let pos1 = crta.points.getItem(crta.points.length - 1);
+        //let posx = pos1.x;
+        //let posy = pos1.y;
+        //console.log("pozicija" + pos1);
+        svg.style.left =  pos + 'px';//element sliding
+          der.style.left =pos + 'px';
+          int.style.left = pos + 'px';
+        console.log(svg.getBoundingClientRect().width);
+        let width = svg.getBoundingClientRect().width + interval;
         console.log(width);
-        event.target.parentElement.style.width = width + 'px';
+        svg.style.width =
+          der.style.width =
+          int.style.width = width + 'px';
         const tocka = svg.createSVGPoint();
         //tocka.x = posx + interval;
-        tocka.x = xcoord;
-        console.log(tocka.x + "=" + posx0 + pos);
+        tocka.x = xcoord + interval;
+        //console.log(tocka.x + "=" + posx0 + pos);
         //tocka.y = posy;
         tocka.y = ycoord;
-        crta.points.appendItem(tocka);
-        console.log("posx" + posx + "posx0" + posx0);
         console.log("xcoord" + xcoord + "ycoord" + ycoord);
+        crta.points.appendItem(tocka);
+        tocka.y -= svg.getBoundingClientRect().height / 2;
+        //console.log("posx" + posx + "posx0" + posx0);
         //crta.points.baseVal.value =pos1-pos;
-        //crta.x1.baseVal.Value=pos;
+        //crta.points += event.xcoord+","+event.ycoord;
+        //console.log(crta.points.getItem(0));
+        //console.log("linija:(" + tocka.x+","+ tocka.y + ")");
+        //console.log(crta.points.getItem(crta.points.length - 1).x);
+        const tocka_der = der.createSVGPoint();
+        //treba uvesti 0 kod uzimanja prve vrijednosti
+        //
+        const y2 = crta.points.getItem(crta.points.length - 1).y;
+        const y1 = crta.points.getItem(crta.points.length - 2).y;
+        const x2 = crta.points.getItem(crta.points.length - 1).x;
+        const x1 = crta.points.getItem(crta.points.length - 2).x;
+        tocka_der.y = ((y2 - y1) / (x2 - x1))*10;
+        //tocka_der.y = (crta.points.getItem(crta.points.length-1).y-crta.points.getItem(crta.points.length-2).y)/(crta.points.getItem(crta.points.length-1).x-crta.points.getItem(crta.points.length-2).x);
+        tocka_der.x = x1+(x2-x1)/ 2;
+        console.log("derivacija:(" + tocka_der.x+","+ tocka_der.y + ")");
+        deriv.points.appendItem(tocka_der);
+        const tocka_int = int.createSVGPoint();
+        tocka_int.y = integ.points.getItem(integ.points.length - 1).y + ((y2 + y1)*(x2-x1)/2)/100;
+        tocka_int.x = x1+(x2-x1)/2;
+        console.log("integracija:(" + tocka_int.x+","+ tocka_int.y + ")");
+        integ.points.appendItem(tocka_int);       //crta.x1.baseVal.Value=pos;
       }
 
 
-      id = setInterval(frame, 1);
+      id = setInterval(frame, 100);
       check = true;
       return;
     }
